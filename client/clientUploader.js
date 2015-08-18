d236de87aeb0a01937766daeed4e42f3818b64a8
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-08-18 19:43:37 -0400

    csv uploads, but cannot be parsed in stream

--- client/clientUploader.js
+++ client/clientUploader.js
@@ -3,8 +3,11 @@ Template.upload.events({
     console.log('file changes');
     FS.Utility.eachFile(event, function(file) {
       Images.insert(file, function (err, fileObj) {
+        console.log(fileObj);
+        console.log(fileObj._id);
         // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
       });
+
     });
   }
 });
--- server/serverUploader.js
+++ server/serverUploader.js
@@ -2,13 +2,31 @@
 
 filePath = process.env.PWD + "/public/data/test.csv";
 parsed = Baby.parseFiles(filePath, {header:true, dynamicTyping: true});
-console.log(parsed.data);
+console.log(parsed);
 
-// Allow Deny rules for collection FS. Do they need to be only on the serverside?
 
-Images.allow({
-  'insert': function () {
-    // add custom authentication code here
-    return true;
-  }
+// Cool reactive util from CollectionFS has a reactive callback to when a file is uploaded.
+Images.on('uploaded', function (fileObj) {
+  // do something
+  console.log(fileObj._id);
+  file = fileObj.createReadStream();
+  console.log(file);
+  // Generating error with parsing the same files as is sucessfully parsed above.
+  parsed = Baby.parseFiles(file.pipe());
+  console.log(parsed);
+  //UploadedFiles.insert({fileid: fileObj._id , name: fileObj.data.blob.name});
+
+
+
 });
+
+
+// Images.deny({
+//    update: function(){
+//    return false;
+//    },
+//    insert: function () {
+//      // add custom authentication code here
+//      return false;
+//    }
+//  });
