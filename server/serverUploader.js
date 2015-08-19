4c495727c7755bd19c05ec9c28f8b2ca6899f9f9
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-08-19 14:27:57 -0400

    documenting issue on serverUploader.js

--- server/serverUploader.js
+++ server/serverUploader.js
@@ -1,32 +1,17 @@
-//Try parsing csv with modweb:baby-parse
+// Parsing a csv directly from filesystem works with modweb:baby-parse
 
 filePath = process.env.PWD + "/public/data/test.csv";
 parsed = Baby.parseFiles(filePath, {header:true, dynamicTyping: true});
 console.log(parsed);
 
-
+// Using CollectionFS requires streaming the data. Somehow the data is not valid.
 // Cool reactive util from CollectionFS has a reactive callback to when a file is uploaded.
 Images.on('uploaded', function (fileObj) {
-  // do something
   console.log(fileObj._id);
-  file = fileObj.createReadStream();
-  console.log(file);
-  // Generating error with parsing the same files as is sucessfully parsed above.
-  parsed = Baby.parseFiles(file.pipe());
-  console.log(parsed);
-  //UploadedFiles.insert({fileid: fileObj._id , name: fileObj.data.blob.name});
+  readStream = fileObj.createReadStream('whatever.csv');
+  writeStream = fileObj.createWriteStream('whatever.csv');
 
-
-
+  readStream.pipe(writeStream)
+  test = Baby.parseFiles(writeStream)
+  console.log(test);
 });
-
-
-// Images.deny({
-//    update: function(){
-//    return false;
-//    },
-//    insert: function () {
-//      // add custom authentication code here
-//      return false;
-//    }
-//  });
