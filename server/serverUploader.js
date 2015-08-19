e3430263720688519c57e5b7e9afd287e1c098fd
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-08-19 14:38:16 -0400

    Better description

--- server/serverUploader.js
+++ server/serverUploader.js
@@ -6,10 +6,14 @@ console.log(parsed);
 
 // Using CollectionFS requires streaming the data. Somehow the data is not valid.
 // Cool reactive util from CollectionFS has a reactive callback to when a file is uploaded.
+// help: https://forums.meteor.com/t/reading-an-uploaded-file-with-collectionfs/8630
 Images.on('uploaded', function (fileObj) {
-  console.log(fileObj._id);
+  //checking that something happens
+  console.log('FileID just uploaded: ' + fileObj._id);
+
+  //create files from streams http://www.sitepoint.com/basics-node-js-streams/#piping
   readStream = fileObj.createReadStream('whatever.csv');
-  writeStream = fileObj.createWriteStream('whatever.csv');
+  writeStream = fileObj.createWriteStream('newwhatever.csv');
 
   readStream.pipe(writeStream)
   test = Baby.parseFiles(writeStream)
