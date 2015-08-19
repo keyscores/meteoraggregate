9d3499b4ea53595a66647f38cdb2182e672abe95
Jeremy Dillworth <jdillworth@gmail.com>
2015-08-19 15:38:39 -0400

    Got data to parse by reading the stream and reassembling a string to hand off to Baby.parse

--- server/serverUploader.js
+++ server/serverUploader.js
@@ -4,18 +4,31 @@ filePath = process.env.PWD + "/public/data/test.csv";
 parsed = Baby.parseFiles(filePath, {header:true, dynamicTyping: true});
 console.log(parsed);
 
+
 // Using CollectionFS requires streaming the data. Somehow the data is not valid.
 // Cool reactive util from CollectionFS has a reactive callback to when a file is uploaded.
 // help: https://forums.meteor.com/t/reading-an-uploaded-file-with-collectionfs/8630
 Images.on('uploaded', function (fileObj) {
   //checking that something happens
-  console.log('FileID just uploaded: ' + fileObj._id);
+  console.log('FileID just uploaed: ' + fileObj._id, fileObj.isUploaded(), fileObj.hasStored());
+  console.dir(fileObj);
 
-  //create files from streams http://www.sitepoint.com/basics-node-js-streams/#piping
-  readStream = fileObj.createReadStream('whatever.csv');
-  writeStream = fileObj.createWriteStream('newwhatever.csv');
-  readStream.pipe(writeStream)
+  console.log('waiting 10s');
+  Meteor.setTimeout(function() {
 
-  test = Baby.parseFiles(writeStream)
-  console.log(test);
+    //create files from streams http://www.sitepoint.com/basics-node-js-streams/#piping
+    var readStream = fileObj.createReadStream();
+    var csvPieces = [];
+    readStream.on('data', function(d) {
+      csvPieces.push(d.toString());
+    });
+    readStream.on('end', function() {
+      console.log('stream end?');
+      var test = Baby.parse(csvPieces.join(''));
+      console.log(test);
+    });
+    // var writeStream = fileObj.createWriteStream('newwhatever.csv');
+    // readStream.pipe(writeStream)
+
+  }, 10000);
 });
